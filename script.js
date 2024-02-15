1d4eccd5a7297782887faeaac4e0e56b57bc6d3f
Radheshyam lodhi <radheshyamlodhi48@gmail.com>
2024-02-15 23:55:38 +0530

    third page done

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("main"),
+  smooth: true,
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("main").style.transform
+    ? "transform"
+    : "fixed",
+});
+
 document.addEventListener("mousemove", function (e) {
   gsap.to("#cursore", {
     top: e.y,
@@ -6,12 +38,12 @@ document.addEventListener("mousemove", function (e) {
   });
 });
 
-gsap.to("video", {
+gsap.to("#page1 video", {
   filter: "blur(20px)",
   transform: "scaleX(0.85)",
   scrollTrigger: {
     trigger: "#page1",
-    scroller: "body",
+    scroller: "main",
     markers: true,
     start: "top 0",
     end: "top -50%",
@@ -24,7 +56,7 @@ gsap.to("#second", {
   duration: 2,
   scrollTrigger: {
     trigger: "#nav",
-    scroller: "body",
+    scroller: "main",
     start: "top 0",
     end: "top -10%",
     scrub: true,
@@ -35,16 +67,31 @@ gsap.to("nav i", {
   display: "block",
   scrollTrigger: {
     trigger: "#nav",
-    scroller: "body",
+    scroller: "main",
     start: "top -15%",
     end: "top -20%",
     scrub: true,
   },
 });
 
-gsap.to("#page2 img", {
+gsap.to("#page2 #imgscroll", {
   transform: "translateY(12%) translateX(75%)",
   duration: 15,
   repeat: -1,
-  ease: "none",
+  ease: 5,
+});
+
+var tl = gsap.timeline();
+tl.from("h2 ,p,button", {
+  y: 200,
+  duration: 1,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "h2,p,button",
+    scroller: "main",
+    markers: true,
+    start: "top 300",
+    end: "top 70",
+    scrub: true,
+  },
 });
